3c597726d62b17178041865914d98c8636ea9693
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-10-06 22:41:34 -0300

    Update create-order.lua

--- _dev/wrk/create-order.lua
+++ _dev/wrk/create-order.lua
@@ -5,19 +5,23 @@ cartUrl = "http://localhost:3001"
 
 customerIds = {
     "068fb99e-290a-4b0c-ba55-fb8f0239eed8",
-    "20d54ed5-c94d-48f7-a912-bd85df84a6b9"
+    "20d54ed5-c94d-48f7-a912-bd85df84a6b9",
+    "ea5a387f-3535-4153-bd36-2e944ccdd459"
 }
 addressIds = {
     "1903e7e5-5cca-4cef-9d58-7bc42a2453d7",
-    "b7cb37d3-0094-483c-88de-b6b6b63c1bc2"
+    "b7cb37d3-0094-483c-88de-b6b6b63c1bc2",
+    "4ed6fbca-9c27-4bd8-a998-ef190e88a8c8"
 }
 cardIds = {
     "42bacbfb-452d-45cd-932e-83a7a2b449ab",
-    "aea3692c-e415-4643-b2a2-08e776cd58ea"
+    "aea3692c-e415-4643-b2a2-08e776cd58ea",
+    "b4086cf9-f8c7-4d32-a48d-f898424301c8"
 }
 cartIds = {
     "f716e351-5df1-4c9c-8324-bd12866e2900",
-    "691c5af7-c651-4f36-b19e-184d37acc481"
+    "691c5af7-c651-4f36-b19e-184d37acc481",
+    "2ca11a65-2c4b-4671-b98a-724d31f457aa"
 }
 
 math.randomseed(os.clock()*100000000000)
@@ -39,7 +43,7 @@ getItemsUrl = function(cartId)
 end
 
 getBody = function()
-    index = 1
+    index = math.random(1, 3)
     return "{\"customerUrl\": \""..getCustomerUrl(customerIds[index]).."\",\"addressUrl\": \""..getAddressUrl(addressIds[index]).."\",\"cardUrl\": \""..getCardUrl(cardIds[index]).."\",\"itemsUrl\": \""..getItemsUrl(cartIds[index]).."\"}"
 end
 
